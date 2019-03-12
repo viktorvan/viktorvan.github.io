97f584bc9a0c0006d46a27f91cb47eb1e33605d5
Viktor Andersson <v.andersson@me.com>
2019-03-13 00:35:21 +0100

    Deploy

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Why you should model your domain with F#",
         "excerpt":"Recently when working on a feature for a client, writing the normal barrage of C# null-checks and unit-tests verifying that my model state wasn’t invalid, a mantra was repeating in my mind. I wish I was coding in F#. Why? Let’s do the exercise of implementing a feature in both...","categories": ["fsharp"],
         "tags": [],
-        "url": "http://localhost:4000/fsharp/domain-modeling-with-fsharp/",
+        "url": "https://viktorvan.github.io/fsharp/domain-modeling-with-fsharp/",
         "teaser":null}]
